6031203da30e85917b45d07cba31b5110f92f130
ijnek <kenjibrameld@gmail.com>
2021-08-15 18:28:01 +0900

    modify install script

--- nao_description/install.sh
+++ nao_description/install.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$0")"
 mkdir tmp
 wget https://github.com/ros-naoqi/nao_meshes_installer/raw/master/naomeshes-0.6.7-linux-x64-installer.run -P tmp/
-chmod +x tmp/naomeshes-0.6.7-linux-x64-installer.run && ./tmp/naomeshes-0.6.7-linux-x64-installer.run  --prefix . --mode text
-mv -t ../  meshes/ texture/
+chmod +x tmp/naomeshes-0.6.7-linux-x64-installer.run && ./tmp/naomeshes-0.6.7-linux-x64-installer.run  --prefix tmp/ --mode text
+mv -t .  tmp/meshes/ tmp/texture/
+rm -rf tmp/
 cd -
